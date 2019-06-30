69f49c38176047daa74433dd1701131d3e26c903
Andherson Maeda <maeda.br@gmail.com>
2019-06-30 22:31:11 +0530

    add max number of cpu cores.

--- settings.py
+++ settings.py
@@ -4,6 +4,9 @@ import torch
 from dotenv import load_dotenv
 load_dotenv(verbose=True)
 
+print("NUM CORES: " + str(torch.get_num_threads()))
+torch.set_num_threads(torch.get_num_threads())
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 BASE_DIR = os.environ.get("BASE_DIR")
